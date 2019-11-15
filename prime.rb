abb65837a674b2798a08415b07dba214784d0783
Owen Cruse <ocruse7@gmail.com>
2019-11-15 05:53:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  modNum = 2
+  
+  if number < 0
+    return false
+  else
+    while modNum < ((number/2) +1) do
+      if number % modNum  == 0 && number != modNum
+        return false
+      else
+        
+        modNum += 1
+      end
+      
+    end
+    true
+  end
+
+end
